168acf446a272d952e1abcb601deadf78768e71c
Mario Bermudez JR <mario.bermudez.jr@outlook.com>
2024-01-09 15:09:03 +0700

    style footer

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -1,6 +1,6 @@
 const SocialMediaIcons = () => {
   return (
-    <div className="flex justify-center md:justify-start my-10 gap-7">
+    <div className="flex justify-center md:justify-start my-3 gap-4">
       <a
         className="hover:opacity-50 transition duration-500"
         href="https://www.linkedin.com/in/mario-bermudez/"
--- src/scenes/Footer.jsx
+++ src/scenes/Footer.jsx
@@ -2,11 +2,11 @@ import SocialMediaIcons from '../components/SocialMediaIcons'
 
 const Footer = () => {
   return (
-    <footer className="h-64 bg-blue pt-10">
-      <div className="w-10/12 mx-auto">
+    <footer className="h-32 bg-blue pt-3">
+      <div className="w-11/12 mx-auto">
         <SocialMediaIcons />
         <div className="md:flex justify-center md:justify-between text-center ">
-          <p className="font-playfair font-semibold text-2xl text-white">MARIO BERMUDEZ</p>
+          <p className="font-playfair font-semibold text-2xl text-white">Mario Bermudez Jr</p>
           <p className="font-playfair text-md text-white">
             ©2023 Curious Loop. All Rights Reserved.
           </p>
