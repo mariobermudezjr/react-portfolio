0c8a72501b0f952d11d8b9df397425d6f882a597
Mario Bermudez JR <mario.bermudez.jr@outlook.com>
2024-01-29 16:00:43 +0700

    Divide and conquer troubleshooting strategy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Navbar from './scenes/Navbar'
 import Landing from './scenes/Landing'
 import DotGroup from './scenes/DotGroup'
-import MySkills from './scenes/MySkills'
+// import MySkills from './scenes/MySkills'
 import LineGradient from './components/LineGradient'
-import Projects from './scenes/Projects'
-import Contact from './scenes/Contact'
+// import Projects from './scenes/Projects'
+// import Contact from './scenes/Contact'
 import Footer from './scenes/Footer'
 import useMediaQuery from './hooks/useMediaQuery'
 import { useEffect, useState } from 'react'
@@ -46,20 +46,19 @@ function App() {
           <Landing setSelectedPage={setSelectedPage} />
         </motion.div>
       </div>
-      <LineGradient />
+      {/* <LineGradient />
       <div className="w-5/6 mx-auto md:h-full ">
         <motion.div
           margin="0 0 -200px 0"
           amount="all"
           onViewportEnter={() => setSelectedPage('skills')}
-        >
-          <MySkills />
-        </motion.div>
-      </div>
-      <div className="pt-20 mt-20">
+        ></motion.div>
+      </div> */}
+      {/* <MySkills /> */}
+      {/* <div className="pt-20 mt-20">
         <LineGradient />
-      </div>
-      <div className="w-5/6 mx-auto">
+      </div> */}
+      {/* <div className="w-5/6 mx-auto">
         <motion.div
           margin="0 0 -200px 0"
           amount="all"
@@ -67,7 +66,7 @@ function App() {
         >
           <Projects />
         </motion.div>
-      </div>
+      </div> */}
       <LineGradient />
       {/* <div className="w-5/6 mx-auto md:h-full">
         <motion.div
@@ -79,7 +78,7 @@ function App() {
         </motion.div>
       </div>
       <LineGradient /> */}
-      <div className="w-5/6 mx-auto md:h-full">
+      {/* <div className="w-5/6 mx-auto md:h-full">
         <motion.div
           margin="0 0 -200px 0"
           amount="all"
@@ -87,7 +86,7 @@ function App() {
         >
           <Contact />
         </motion.div>
-      </div>
+      </div> */}
       <Footer />
     </div>
   )
